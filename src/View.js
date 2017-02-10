4436979ca8f392f83cff7c01fc98dab43b122744
pmros <patricioros.dev@gmail.com>
2017-02-10 10:21:32 +0100

    Filter undefined elements and let arrays as vdom

--- src/View.js
+++ src/View.js
@@ -1,9 +1,11 @@
-import {Component, Chain, Mapper, Accumulator, Hub} from 'graflow'
+import {Component, Chain, Mapper, Accumulator, Hub, Filter} from 'graflow'
 
 const setComponent = (vdom, component) => {
-  if(typeof vdom === 'object' && !vdom.component) vdom.component = component
-  if(Array.isArray(vdom.content)) {
-    vdom.content.map(e => setComponent(e, component))
+  if(Array.isArray(vdom)) {
+    vdom.map(e => setComponent(e, component))
+  } else {
+    if(typeof vdom === 'object' && !vdom.component) vdom.component = component
+    if(vdom.content) setComponent(vdom.content, component)
   }
   return vdom
 }
@@ -14,7 +16,8 @@ const View = (view, component) => Component({
     vdomAccumulator: Accumulator(),
     hub: Hub('vdom', 'state'),
     globalAccumulator: Accumulator(),
-    view: Mapper(({state, vdom}) => view(state, vdom)),
+    view: Mapper(({state = {}, vdom = {}}) => view(state, vdom)),
+    filter: Filter(v => v !== undefined),
     post: Mapper(vdom => setComponent(vdom, component))
   },
   connections: [
@@ -23,7 +26,7 @@ const View = (view, component) => Component({
     ['in.state', 'hub.state'],
     ['hub', 'globalAccumulator'],
     ['globalAccumulator', 'view'],
-    ['view', 'post'],
+    ['view', 'filter'], ['filter', 'post'],
     ['post', 'out']
   ]
 })
